0c3b8f5316ca6eb9b7946b0652b839e59822ac8a
bche0078 <bche0078@student.monash.edu>
2025-03-20 14:23:12 +1100

    Hosting local database to cloud database

--- vue-project/backend/database.py
+++ vue-project/backend/database.py
@@ -1,14 +1,29 @@
+# # Local connection
+# import psycopg2
+# import os
+# from dotenv import load_dotenv
+# # Get environment variables file(.env)
+# load_dotenv()
+# # Test on the PostgreSQL connection
+# def get_db_connection():
+#     return psycopg2.connect(
+#         host=os.getenv("DB_HOST"),
+#         database=os.getenv("DB_NAME"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASSWORD"),
+#         port=os.getenv("DB_PORT")
+#     )
+
+# Remote connection
 import psycopg2
-import os
-from dotenv import load_dotenv
-# Get environment variables file(.env)
-load_dotenv()
-# Test on the PostgreSQL connection
+
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
+    conn = psycopg2.connect(
+        dbname="FIT5120IE",
+        user="postgres",
+        password="bche0078",
+        # Public IP from Cloud Instance
+        host="34.129.0.237",
+        port="5432"
     )
+    return conn
