950aa15c9eec6e4eb4e4a2c06d0342a4ce252387
bche0078 <bche0078@student.monash.edu>
2025-03-20 15:21:47 +1100

    Remote testing

--- vue-project/backend/database.py
+++ vue-project/backend/database.py
@@ -14,16 +14,38 @@
 #         port=os.getenv("DB_PORT")
 #     )
 
-# Remote connection
+# Gould cloud SQL remote connection
+# import psycopg2
+
+# def get_db_connection():
+#     conn = psycopg2.connect(
+#         dbname="FIT5120IE",
+#         user="postgres",
+#         password="bche0078",
+#         # Public IP from Cloud Instance
+#         host="34.129.0.237",
+#         port="5432"
+#     )
+#     return conn
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
+    # Get database credentials from environment variables
+    dbname = os.getenv('DB_NAME')
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    connection_name = os.getenv('CLOUD_SQL_CONNECTION_NAME')
+
+    # Connect using Cloud SQL instance socket
     conn = psycopg2.connect(
-        dbname="FIT5120IE",
-        user="postgres",
-        password="bche0078",
-        # Public IP from Cloud Instance
-        host="34.129.0.237",
-        port="5432"
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=f'/cloudsql/{connection_name}',
     )
     return conn
